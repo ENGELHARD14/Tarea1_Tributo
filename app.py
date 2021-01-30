cff5d9233336b25a616639fcadcb72d5b36456a0
Ricardo Engelhard <72060532+ENGELHARD14@users.noreply.github.com>
2021-01-29 19:42:09 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__
+@app.route("/")
+def home():
+    return render_template("index.htm")
+
+@app.route("/conduccionTemeraria")
+def about():
+    return render_template("conduccionTemeraria.htm")
+
+if __name__ == "__main__":
+    app.run(debug=True)
